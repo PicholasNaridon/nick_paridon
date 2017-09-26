d5d4f4e2d809de0acc696d752cecb96e32684cb8
Nick Paridon <paridonn1@gmail.com>
2017-09-26 16:32:45 -0400

    works migration, controller, model and some views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926135838) do
+ActiveRecord::Schema.define(version: 20170926201018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20170926135838) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "languages"
+    t.string "link"
+    t.string "git"
   end
 
 end
