5c4aacdad95d2a2214900e9a1e77a9d85f05b7f6
Nick Paridon <paridonn1@gmail.com>
2017-09-27 14:25:32 -0400

    worksControllerSpec fix

--- spec/controllers/work_controller_spec.rb
+++ spec/controllers/work_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe WorkController, type: :controller do
+RSpec.describe WorksController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
